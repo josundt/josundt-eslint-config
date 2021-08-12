b2741d4a0fb44ee7ba2df8457f0c47675fadec5e
Jørn Andre Sundt <jsundt@trintech.com>
2021-08-12 12:03:04 +0200

    Added stricter import rules

--- rules/import-typescript.js
+++ rules/import-typescript.js
@@ -6,6 +6,12 @@ module.exports = {
         "import"
     ],
     "rules": {
+        "import/extensions": [ // Ensure all local .ts file imports use .js extension
+            "error",
+            "always"
+        ],
+        "import/no-commonjs": "error", // Disallow require() syntax - only esm syntax allowed
+        "import/no-nodejs-modules": "error", // Disallowed: import * as path from "path"; Allowed: import * as path from "node:path";
         "import/no-default-export": "off",
         "import/no-deprecated": "error",
         "import/no-extraneous-dependencies": "off",
